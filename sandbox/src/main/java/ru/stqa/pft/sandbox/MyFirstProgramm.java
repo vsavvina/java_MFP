af57e46c15cab92a20999304ab13c0f0b91d8aac
vsavvina <82946852+vsavvina@users.noreply.github.com>
2021-04-23 15:11:10 +0300

    Работа с функциями, используемыми друг в друге

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -4,6 +4,21 @@ public class MyFirstProgramm
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        hello("Mir");
+        hello("Mur");
+
+        double i = 4.0;
+        double l = 3.0;
+        System.out.println("Площадь прямоугольного треугольника со сторонами "+i+" и "+l+" = "+area(i,l));
+    }
+
+    public static void hello(String word)
+    {
+        System.out.println("Hello, " + word);
+    }
+
+    public static double area(double m, double n)
+    {
+        return (m*n)/2;
     }
 }
