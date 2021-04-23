b816877194b1a1a041aa5f88fb06bacdb6447726
vsavvina <82946852+vsavvina@users.noreply.github.com>
2021-04-23 16:39:24 +0300

    Перенос методов объектов в соответствующие классы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -8,22 +8,13 @@ public class MyFirstProgramm
         hello("Mur");
 
         Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной "+s.l+" = "+area(s));
+        System.out.println("Площадь квадрата со стороной "+s.l+" = "+s.area());
 
         Rectangle r = new Rectangle(4,3);
-        System.out.println("Площадь прямоугольного треугольника со сторонами "+r.a+" и "+r.b+" = "+area(r));
+        System.out.println("Площадь прямоугольного треугольника со сторонами "+r.a+" и "+r.b+" = "+r.area());
     }
-
     public static void hello(String word)
     {
         System.out.println("Hello, " + word);
-    }
-    public static double area(Square s)
-    {
-        return s.l*s.l;
-    }
-    public static double area(Rectangle r)
-    {
-        return (r.a*r.b)/2;
     }
 }
