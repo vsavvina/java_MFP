eef56c5cb15ab6bc056bd75e794cff059ea9b4e0
vsavvina <82946852+vsavvina@users.noreply.github.com>
2021-05-07 11:41:44 +0300

    Введение критериев подбора кредита

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/VhodsParametrami.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/VhodsParametrami.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.fail;
 
-public class VhodvKK {
+public class VhodsParametrami {
   private WebDriver wd;
   private String baseUrl;
   private boolean acceptNextAlert = true;
@@ -21,42 +21,44 @@ public class VhodvKK {
     wd = new FirefoxDriver();
     baseUrl = "https://www.google.com/";
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    login("iz001", "123");
-  }
-
-  private void login(String username, String pass)
-  {
-    wd.get("http://front.kubiq.do.neoflex.ru/auth");
-    wd.findElement(By.id("login")).click();
-    wd.findElement(By.id("login")).clear();
-    wd.findElement(By.id("login")).sendKeys(username);
-    wd.findElement(By.xpath("//div[@id='root']/div[2]/div/div/div/div[2]")).click();
-    wd.findElement(By.id("password")).click();
-    wd.findElement(By.id("password")).clear();
-    wd.findElement(By.id("password")).sendKeys(pass);
-    wd.findElement(By.xpath("//div[@id='root']/div[2]/div/div/div")).click();
-    wd.findElement(By.xpath("//div[@id='root']/div[2]/div/div/div/div[2]/div[3]/button/span")).click();
   }
 
   @Test
-  public void testVhodvKK() throws Exception
-  {
-
+  public void testVhodsParametrami() throws Exception {
+    wd.get("http://front.kubiq.do.neoflex.ru/auth");
+    wd.findElement(By.id("login")).click();
+    wd.findElement(By.id("login")).clear();
+    wd.findElement(By.id("login")).sendKeys("iz001");
+    wd.findElement(By.xpath("//div[@id='root']/div[2]/div/div/div")).click();
+    wd.findElement(By.id("password")).click();
+    wd.findElement(By.id("password")).clear();
+    wd.findElement(By.id("password")).sendKeys("123");
+    wd.findElement(By.xpath("//div[@id='root']/div[2]/div/div/div/div[2]")).click();
+    wd.findElement(By.xpath("//div[@id='root']/div[2]/div/div/div/div[2]/div[3]/button/span")).click();
+    wd.findElement(By.id("calculator")).click();
+    Thread.sleep(1000);//задержка на отрисовку и выполнение
+    wd.findElement(By.xpath("//div[text()='Автокредит']")).click();//если не написан однозначный идентификатор, но есть текст в классе div
+    Thread.sleep(1000);
+    wd.findElement(By.xpath("//input[@name='carProductionDate']")).click();//если идентификатор в классе input
+    wd.findElement(By.xpath("//input[@name='carProductionDate']")).clear();
+    wd.findElement(By.xpath("//input[@name='carProductionDate']")).sendKeys("2017");
+    Thread.sleep(1000);
+    wd.findElement(By.xpath("//input[@name='carPrice']")).click();
+    wd.findElement(By.xpath("//input[@name='carPrice']")).clear();
+    wd.findElement(By.xpath("//input[@name='carPrice']")).sendKeys("200 000");
+    Thread.sleep(1000);
+    wd.findElement(By.xpath("//div[@id='root']/div/div[2]/div[2]/div/div/div/div[2]/div/div[2]/div/div/div/form/div/div/div/div/div/div/div/div[3]/div/button/span")).click();
+    wd.findElement(By.xpath("//img[@alt='Exit button']")).click();
+    Thread.sleep(1000);
   }
 
   @AfterClass(alwaysRun = true)
-  public void tearDown() throws Exception
-  {
-    exfromkk();
+  public void tearDown() throws Exception {
     wd.quit();
     String verificationErrorString = verificationErrors.toString();
     if (!"".equals(verificationErrorString)) {
       fail(verificationErrorString);
     }
-  }
-  private void exfromkk()
-  {
-    wd.findElement(By.xpath("//img[@alt='Exit button']")).click();
   }
 
   private boolean isElementPresent(By by) {
